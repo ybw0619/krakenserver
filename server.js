214f3ac1409e4cfef328946b6bc563ea417c0a8b
5주뇽 <ojy9406@naver.com>
2019-12-27 13:36:54 +0900

    역할 추가 role 에 배열로 넣음
    3~4 1장고정 5~6인 1~2장 7~8인 2~3장

--- server.js
+++ server.js
@@ -39,11 +39,26 @@ io.on('connection', (socket) => {
     //빈상자 (4n-1만큼)
     empty = Array(4 * numberOfPlayer - 1).fill('빈상자')
 
+    //플레이어수 4인 이하 유령1명고정
+    //5~6인 1~2, 7~8인 2~3
+    if(numberOfPlayer >= 5){
+      ghostcount = Math.ceil(numberOfPlayer/2)-2 + parseInt(Math.random()*10)%2
+      ghost = Array(get).fill("유령")
+    } else {
+      ghost = ["유령"]
+    }
+    //유령수 확정후 나머지 선원
+    pirate = Array(numberOfPlayer-ghost.length).fill("해적")
+    //유령,해적 역할 합치기
+    role = [...ghost, ...pirate]
+
     //크라켄 + 보물상자 + 빈상자 한 덱으로 합치고
     deck = [...craken, ...gold, ...empty]
 
     //덱을 섞어 섞어
     deck.sort(() => Math.random() - Math.random())
+    //역할배정 섞기
+    role.sort(() => Math.random() - Math.random())
 
     //플레이어는 최대 8명
     player = [[],[],[],[],[],[],[],[]]
@@ -53,8 +68,7 @@ io.on('connection', (socket) => {
 
     for (let i = 0; i < numberOfPlayer; i++) {
       console.log(i);
-      
-      io.to(userList[i].id).emit('role', {id: userList[i].id, role: 'ghost'})
+      io.to(userList[i].id).emit('role', {id: userList[i].id, role: role[i]})
       io.to(userList[i].id).emit('deck', player[i])
       io.emit('others', {user: userList[i].id, deckLength: player[i].length})
       io.emit('turn-start', userList[0])
