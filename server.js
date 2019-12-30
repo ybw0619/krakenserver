da187e3de6a8a1b2f5327421288a3d9b07b01c16
양호준 <ybw0619@naver.com>
2019-12-30 18:42:30 +0900

    -
    -

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ io.on('connection', (socket) => {
 
   //선택
   socket.on('pick', ({i,j}) => {
-    console.log('픽', i, j);
+    console.log('픽 : ', i+1,'번 플레이어의 ', j+1, '번째 카드를 골랐습니다.');
     
     //덱에서 카드 뽑고
     pickedCard = game.users[i].deck.splice(j, 1)[0]
@@ -118,79 +118,20 @@ io.on('connection', (socket) => {
         io.emit('gameEnd', '탐험대 승리')
       }
     }
-    else if(pickedCard == '빈상자'){
-      if(game.turn >= numberOfPlayer*4){
-        io.emit('game', game)
-        io.emit('gameEnd', '스켈레톤 승리')
-      }
-    }
     game.turn++
     game.round = parseInt(game.turn/numberOfPlayer)
     game.pickedCards.push(pickedCard) 
-    console.log(game);
-    
+
+    if (game.turn%numberOfPlayer==0){
+      console.log(game.round+1,' 라운드 입니다.');
+      
+      roundInit()
+    }
+    if (game.turn >= numberOfPlayer * 4 - 1) {
+      io.emit('game', game)
+      io.emit('gameEnd', '스켈레톤 승리')
+    }
     io.emit('game', game)
-  })
-  
-
-  //호준 채팅 (chat이벤트로 날아온데이터 전체한테 다시 뿌려주기.)
-  socket.on('chat', (data) => {
-    io.emit('chat', data)
-  })
-
-  
-  socket.on('turn-select', (turn) => {
-    nowTurnId = turn.id
-    let nowSelect = turn.selectCard //선택한 카드의 인덱스
-
-    userList.forEach((user,i) => {
-      if (user.id === turn.id) {
-        si = i
-      }
-    })
-
-    //카드 체크 -오준용
-    cardCheck(turn,si);
-
-    player[si].splice(turn.selectCard, 1)
-    
-    nowTurn = nowTurn + 1
-
-    console.log('nowTurn',nowTurn)
-
-    for (let i = 0; i < numberOfPlayer; i++) {
-      io.emit('turn-start', {id: nowTurnId, turn: nowTurn})
-      io.emit('turn-end', {user: turn.id, deckLength: player[i].length, select: turn.selectCard})
-    }
-  })
-
-  socket.on('round-end', (turn) => {
-    nowTurn = 1
-    round = round + 1
-
-    let temp = []
-
-    player.forEach(deck => {
-      deck.forEach(card => {
-        temp.push(card)
-      })
-    })
-
-    // 라운드 끝났으니까 다시 섞어섞어
-    temp.sort(() => Math.random() - Math.random())
-
-    //플레이어는 최대 8명
-    player = [[],[],[],[],[],[],[],[]]
-
-    //섞은 덱에서 다섯장씩 순서대로 분배분배
-    player = player.map(x => temp.splice(0, 5 - (round - 1)))
-
-    for (let i = 0; i < numberOfPlayer; i++) {
-      io.to(userList[i].id).emit('deck', player[i])
-    }
-    for (let i = 0; i < numberOfPlayer; i++) {
-      io.emit('others', {user: userList[i].id, deckLength: player[i].length})
-    }
   })
 
   socket.on('disconnect', () => {
@@ -204,34 +145,6 @@ server.listen(port, () => {
   console.log('Server On !');
 });
 
-//선택한 카드 확인 함수
-cardCheck = (turn,playerIndex) => {
-  //크라켄 선택 ==> 유령 승리
-  if(player[playerIndex][turn.selectCard]  === "크라켄") {
-    console.log("크라켄 등장");
-  }
-
-  //보물상자 선택
-  else if(player[playerIndex][turn.selectCard]  === "보물상자") {
-    console.log("보물상자 등장");
-    goldCount++;
-    console.log("찾은 보물상자 수 : ",goldCount, "총 보물상자 수 : ", numberOfPlayer);
-    //보물상자 수 == 플레이어 수 (게임종료, 해적 승리)
-    if(goldCount >= numberOfPlayer){
-      console.log("보물상자 모두 찾음 해적 승리");
-    }
-  }
-  //빈상자 선택
-  else if(player[playerIndex][turn.selectCard]  === "빈상자") {
-    console.log("빈상자 입니다");
-  }
-
-  //이상한거 선택시
-  else {
-    console.log("==== CARD SELECT ERROR ====")
-  }
-}
-
 //원래 쓰던 소팅보다 조금더 신뢰성이 높다고 함
 //https://medium.com/@kimploo/%EB%B0%94%EB%9E%8C%EC%A7%81%ED%95%9C-%EB%AC%B4%EC%9E%91%EC%9C%84-%EB%B0%B0%EC%97%B4-fitting-random-array-in-javascript-882d69d49e23
 //링크참조
@@ -240,4 +153,11 @@ function shuffleArray(array) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
   }
+}
+
+//라운드 넘어 갈때마다 플레이어들의 남은 카드 회수해서 섞고 무작위로 재분배
+function roundInit(){
+  let tempDeck = game.users.flatMap(x => x.deck)
+  shuffleArray(tempDeck)
+  game.users.map(x=>x.deck=tempDeck.splice(0, 5-game.round))
 }
